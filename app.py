57b2f42a9a8153726109d2af2ef542b1fda1e85d
leogum <45690704+leogum@users.noreply.github.com>
2020-02-17 16:16:54 -0300

    removing some comments

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from blueprints.bdt.bdt import bdt
 from blueprints.file.file_routes import files_part
 from blueprints.numeracao.numeracao import numeracao
 from blueprints.users.users import users
@@ -9,7 +8,6 @@ from blueprints.users.users import users
 app = Flask(__name__)
 app.register_blueprint(users)
 app.register_blueprint(numeracao)
-app.register_blueprint(bdt)
 app.register_blueprint(files_part)
 app.config.from_pyfile('app_config.py')
 
