33c296306ffeb45f77648d1043720789eb11e8d1
leogum <45690704+leogum@users.noreply.github.com>
2020-02-18 14:00:41 -0300

    *

--- app_config.py
+++ app_config.py
@@ -3,9 +3,8 @@ from datetime import timedelta
 from configs import Configs
 
 conn_str = Configs.get_conn_str()
+SECRET_KEY = Configs.get_secret_key()
+UPLOAD_PATH = Configs.get_upload_path()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = conn_str
-UPLOAD_PATH = Configs.get_upload_path()
-# UPLOAD_FOLDER = 'c:\\uploads_numeracao'
-SECRET_KEY = "case"
 PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
